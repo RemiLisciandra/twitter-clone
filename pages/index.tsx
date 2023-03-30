d961afebcdbcc7fa9b85535d0b2850c66850dbf5
Rémi LISCIANDRA <remilisciandra@MacBook-Pro-de-Remi.local>
2023-03-30 14:43:50 +0200

    Mise en place de l'input et du LoginModal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Header from '../components/Header';
+import Head from 'next/head';
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Home</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
             <Header label="Accueil"></Header>
         </>
     );
